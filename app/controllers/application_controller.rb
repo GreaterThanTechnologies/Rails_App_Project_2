53a9540ba6536b05f3cb6514a516c425d1573ee2
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-03-30 18:12:41 -0400

    adds application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method(:current_owner)
+
+  def current_owner
+    @current_owner = Owner.find_by(id: session[:owner_id])
+  end
+
+  def req_login
+    redirect_to '/login' if !current_owner
+  end
+
+end
 end
