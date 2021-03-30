038012b255103f644103149152f6cc8b19f6c9ec
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-03-30 18:20:31 -0400

    sets items and owners controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,74 @@
 class ItemsController < ApplicationController
+  before_action(:set_item, except: [:index, :new, :create])
+  before_action(:req_login)
+
+  layout "application"
+
+  def index
+    if params[:owner_id]
+      owner = Owner.find_by(id: params[:owner_id])
+      @items = owner.items
+     else
+      @items = Item.all
+    end
+  end
+
+  def show
+  end
+  
+  def new
+    @item = Item.new
+    @item.inventories.build(owner: current_owner)
+    @inventories = @item.inventories.select{|p| p.owner_id == current_owner.id}
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.inventories.each do |p|
+      p.owner = current_owner
+    end
+    if @item.save
+        redirect_to item_path(@item)
+    else
+      @errors = @item.errors.full_messages
+      @inventories = @item.inventories.select{|p| p.owner_id == current_owner.id}
+      render :new
+    end
+  end
+
+  def edit
+    @inventories = @item.inventories.where(owner_id: current_owner.id)
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to(item_path(@item))
+    else
+      @inventories = @item.inventories.select do |p|
+        p.owner_id == current_owner.id
+      end
+      @errors = @item.errors.full_messages
+      @inventories = @item.inventories.select{|p| p.owner_id == current_owner.id}
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @item.delete
+    redirect_to items_path
+  end
+
+####
+  private
+
+    def item_params
+      params.require(:item).permit(:name, inventories_attributes: [:unit, :quantity, :owner_id])
+    end
+
+    def set_item
+      @item = Item.find_by(id: params[:id], name: params[:name])
+    end
+
+
 end
--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,30 @@
 class OwnersController < ApplicationController
+  before_action(:req_login, except: [:new, :create])
+
+  def new
+    @owner = Owner.new
+  end
+
+  def home
+  end
+
+  def create
+    @owner = Owner.new(owner_params)
+    if @owner.save
+      session[:owner_id] = @owner.id
+      redirect_to owner_items_path(@owner)
+    else
+      @errors = @owner.errors.full_messages
+      render :new
+    end
+  end
+
+
+
+
+  private
+
+  def owner_params
+    params.require(:owner).permit(:username, :fname, :lname, :company_name, :password, :location, :password_confirmation)
+  end
 end
