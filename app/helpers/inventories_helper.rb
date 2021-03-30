1ef70581e4ebf87f4376d3fec176a613798e1a15
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-03-30 18:43:55 -0400

    add inventory helper

--- app/helpers/inventories_helper.rb
+++ app/helpers/inventories_helper.rb
@@ -1,2 +1,10 @@
 module InventoriesHelper
+  def show_item_name(m, index)
+    m.item.name if index
+  end
+
+
+  def form_url_helper(item)
+    item ? item_inventories_path(item) : inventories_path
+  end
 end
