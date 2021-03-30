a2b61d43cf44fd10a6c7306f8e2e5984dc613dae
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-03-30 18:09:00 -0400

    migrates

--- db/migrate/20210330220512_create_owners.rb
+++ db/migrate/20210330220512_create_owners.rb
@@ -1,6 +1,12 @@
 class CreateOwners < ActiveRecord::Migration[6.1]
   def change
     create_table :owners do |t|
+      t.string :fname
+      t.string :lname
+      t.string :username
+      t.string :company_name
+      t.string :password_digest
+      t.string :location
 
       t.timestamps
     end
--- db/migrate/20210330220550_create_inventories.rb
+++ db/migrate/20210330220550_create_inventories.rb
@@ -1,6 +1,10 @@
 class CreateInventories < ActiveRecord::Migration[6.1]
   def change
     create_table :inventories do |t|
+      t.string :unit 
+      t.integer :quantity
+      t.integer :item_id
+      t.integer :owner_id
 
       t.timestamps
     end
