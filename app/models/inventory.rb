e21039a1cacb075448c863041c39168a8edff7cf
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-03-30 18:11:12 -0400

    adds models

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,7 @@
 class Inventory < ApplicationRecord
+  belongs_to :item
+  belongs_to :owner
+  validates_presence_of :quantity, :unit
+
+  scope(:quantity_search, ->(quantity) { self.where("quantity >= ?", quantity) })
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  has_many :inventories
+  has_many :owners, through: :inventories
+
+  accepts_nested_attributes_for :inventories
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
